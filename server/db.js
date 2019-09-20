a8e1fc5d2f868d05f0f3b380df3a94d3f2c329bf
ahndohun <ahndohun1@gmail.com>
2019-09-20 19:41:31 +0900

    공부3

--- server/db.js
+++ server/db.js
@@ -1,27 +1,27 @@
-const people = [
+let people = [
   {
     id: 1,
-    name: '이정도',
-    username: 'jd1386',
-    email: 'lee.jungdo@gmail.com',
-    phone: '010-3192-2910',
-    website: 'https://leejungdo.com'
+    name: "이정도",
+    username: "jd1386",
+    email: "lee.jungdo@gmail.com",
+    phone: "010-3192-2910",
+    website: "https://leejungdo.com"
   },
   {
     id: 2,
-    name: '김재완',
-    username: 'lastrites2018',
-    email: 'jaewan@gmail.com',
-    phone: '02-879-5000',
-    website: 'https://github.com/lastrites2018'
+    name: "김재완",
+    username: "lastrites2018",
+    email: "jaewan@gmail.com",
+    phone: "02-879-5000",
+    website: "https://github.com/lastrites2018"
   },
   {
     id: 3,
-    name: '김성은',
-    username: 'sunnysid3up',
-    email: 'sunny@daum.net',
-    phone: '010-4280-1991',
-    website: 'https://github.com/sunnysid3up'
+    name: "김성은",
+    username: "sunnysid3up",
+    email: "sunny@daum.net",
+    phone: "010-4280-1991",
+    website: "https://github.com/sunnysid3up"
   }
 ];
 
@@ -39,6 +39,12 @@ export const modifyPerson = (id, person) => {
   return person;
 };
 export const deletePerson = id => {
-  people = people.filter(person => person.id !== Number(id));
-  return true;
+  let deletedPeople = people.filter(person => person.id !== Number(id));
+
+  if (deletedPeople.length !== people.length) {
+    people = deletedPeople;
+    return true;
+  } else {
+    return false;
+  }
 };
--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-import { GraphQLServer } from 'graphql-yoga';
-import { resolvers } from './resolver';
+import { GraphQLServer } from "graphql-yoga";
+import { resolvers } from "./resolver";
 
 const typeDefs = `
-input person {
-    name: String!
-    username: String!
-    email: String
-    phone: String
-    website: String
-}
+  input person {
+      name: String!
+      username: String!
+      email: String
+      phone: String
+      website: String
+  }
+  
   type people {
     id: ID!
     name: String
@@ -22,6 +23,7 @@ input person {
     people: [people]!
     person(id: ID!): [people]!
   }
+  
   type Mutation{
     addPerson(person: person): people!
     modifyPerson(id: ID!, person: person): people!
@@ -30,4 +32,4 @@ input person {
 `;
 
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log('Server is running on localhost:4000'));
+server.start(() => console.log("Server is running on localhost:4000"));
